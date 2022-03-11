c530b24512dedbe212b5c0ae8d43578ec4a675e7
권은경 <gms02294@gmail.com>
2022-03-12 01:46:26 +0900

    modify

--- inventory_distribution.py
+++ inventory_distribution.py
@@ -1,41 +1,51 @@
+
+
 class Inventory_distribution:
     def order():
         try:
             order_list = {}
-            item = ['사과', '배', '오렌지', '바나나']
+            item = ['사과', '오렌지', '바나나']
             while True:
                 key = input('품목 : ')
+                if key not in item :
+                    print ('존재하지 않는 상품 입니다.')
+                    break
                 val = int(input('수량 : '))
                 order_list[key] = val
-                answer = input('q를 누르면 종료, 계속 주문하고 싶으면 아무 키나 누르세요')
+                answer = input('q를 누르면 종료, 계속 주문하고 싶으면 아무 키나 누르세요.')
                 if answer =='q' :
                     break 
             return order_list
         except ValueError :
             print ('잘못된 정보 입니다.')
     print(order())
-# 키값 중복시 벨류 합치기
-# 정해진 품목 외에 값 들어오면 에러 반환
+# 에러 반환
+# 딕셔너리 특성 상 같은 key값이 들어올 시 value가 수정됨 
+
+
     def get_inventory():
         warehouse_list =[]
         while True :
             name = input('이름 : ')
-            key = input('품목 : ')
-            val = int(input('수량 : '))
-            order = {}
-            order[key] = val
+            while True :
+                order = {}
+                key = input('품목 : ')
+                val = int(input('수량 : '))
+                order[key] = val
+                answer = input('q를 누르면 종료, 재고를 추가하고 싶으면 아무 키나 누르세요')
+                if answer =='q' :
+                    break
             warehouse = {
                 '이름' : name,
                 '재고' : order
                 }
-            answer = input('q를 누르면 종료, 계속 입력하고 싶으면 아무 키나 누르세요')
+            warehouse_list.append(warehouse)
+            answer = input('q를 누르면 종료, 새로운 창고를 추가하려면 아무 키나 누르세요')
             if answer =='q' :
                 break
-            else :
-                warehouse_list.append(warehouse)
         return warehouse_list
     print (get_inventory())
 # # [ { 이름: owd, 재고: { 사과: 5, 주황색: 10 } }, { 이름: dm:, 재고: { 바나나: 5, 주황색: 10 } } ]
-# 키값 중복시 벨류 합치기 
+# 창고이름 중복시 벨류 합치기 
 # 에러 반환
-# 와 마지막 입력은 씹히는지 
+# 품목 여러개 추가하기 
---
+++ shipments.py
@@ -0,0 +1,6 @@
+class InventoryAllocator :
+    def inventory_distribution(order, inventory):
+        result = []
+        for i in range(len(inventory)):
+            stock = list(inventory[i]['inventory'].keys())
+        
