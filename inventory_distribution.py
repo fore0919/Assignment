5e257121fdb06e29bc9152e2b234b1d83367bc5c
fore0919 <gms02294@gmail.com>
2022-03-19 23:34:14 +0900

    add:shipments

--- inventory_distribution.py
+++ inventory_distribution.py
@@ -1,51 +1,49 @@
+# class Inventory_distribution:
+#     def order():
+#         try:
+#             order_list = {}
+#             item = ['사과', '오렌지', '바나나']
+#             while True:
+#                 key = input('품목 : ')
+#                 if key not in item :
+#                     print ('존재하지 않는 상품 입니다.')
+#                     break
+#                 val = int(input('수량 : '))
+#                 order_list[key] = val
+#                 answer = input('q를 누르면 종료, 계속 주문하고 싶으면 아무 키나 누르세요.')
+#                 if answer =='q' :
+#                     break 
+#             return order_list
+#         except ValueError :
+#             print ('잘못된 정보 입니다.')
+#     print(order())
+# # 에러 반환
+# # 딕셔너리 특성 상 같은 key값이 들어올 시 value가 수정됨 
 
 
-class Inventory_distribution:
-    def order():
-        try:
-            order_list = {}
-            item = ['사과', '오렌지', '바나나']
-            while True:
-                key = input('품목 : ')
-                if key not in item :
-                    print ('존재하지 않는 상품 입니다.')
-                    break
-                val = int(input('수량 : '))
-                order_list[key] = val
-                answer = input('q를 누르면 종료, 계속 주문하고 싶으면 아무 키나 누르세요.')
-                if answer =='q' :
-                    break 
-            return order_list
-        except ValueError :
-            print ('잘못된 정보 입니다.')
-    print(order())
-# 에러 반환
-# 딕셔너리 특성 상 같은 key값이 들어올 시 value가 수정됨 
-
-
-    def get_inventory():
-        warehouse_list =[]
-        while True :
-            name = input('이름 : ')
-            while True :
-                order = {}
-                key = input('품목 : ')
-                val = int(input('수량 : '))
-                order[key] = val
-                answer = input('q를 누르면 종료, 재고를 추가하고 싶으면 아무 키나 누르세요')
-                if answer =='q' :
-                    break
-            warehouse = {
-                '이름' : name,
-                '재고' : order
-                }
-            warehouse_list.append(warehouse)
-            answer = input('q를 누르면 종료, 새로운 창고를 추가하려면 아무 키나 누르세요')
-            if answer =='q' :
-                break
-        return warehouse_list
-    print (get_inventory())
-# # [ { 이름: owd, 재고: { 사과: 5, 주황색: 10 } }, { 이름: dm:, 재고: { 바나나: 5, 주황색: 10 } } ]
-# 창고이름 중복시 벨류 합치기 
-# 에러 반환
-# 품목 여러개 추가하기 
+#     def get_inventory():
+#         warehouse_list =[]
+#         while True :
+#             name = input('이름 : ')
+#             while True :
+#                 order = {}
+#                 key = input('품목 : ')
+#                 val = int(input('수량 : '))
+#                 order[key] = val
+#                 answer = input('q를 누르면 종료, 재고를 추가하고 싶으면 아무 키나 누르세요')
+#                 if answer =='q' :
+#                     break
+#             warehouse = {
+#                 '이름' : name,
+#                 '재고' : order
+#                 }
+#             warehouse_list.append(warehouse)
+#             answer = input('q를 누르면 종료, 새로운 창고를 추가하려면 아무 키나 누르세요')
+#             if answer =='q' :
+#                 break
+#         return warehouse_list
+#     print (get_inventory())
+# # # [ { 이름: owd, 재고: { 사과: 5, 주황색: 10 } }, { 이름: dm:, 재고: { 바나나: 5, 주황색: 10 } } ]
+# # 창고이름 중복시 벨류 합치기 
+# # 에러 반환
+# # 품목 여러개 추가하기 
--- shipments.py
+++ shipments.py
@@ -1,6 +1,33 @@
-class InventoryAllocator :
+from curses import is_term_resized
+from lib2to3.pgen2.token import ISTERMINAL
+
+
+class InventoryAllocator :  
     def inventory_distribution(order, inventory):
         result = []
-        for i in range(len(inventory)):
-            stock = list(inventory[i]['inventory'].keys())
-        
+
+        for i in range(len(inventory)): # 창고에 있는 품목을 뽑음
+            stock = list(inventory[i]['inventory'].keys()) # 재고 (중첩)딕셔너리에서 품목들(키)을 리스트로 받아서 stock에 할당 
+            output = {}
+
+            # for item in order and stock : 
+            # count = (inventory[i]['inventory'].get(item)) 
+
+
+
+
+# Input: { apple: 10 }, [ { name: owd, inventory: { apple: 5, orange: 10 } }, { name: dm, inventory: { banana: 5, orange: 10 } } ]
+# Output: [{ dm: { apple: 5 }}, { owd: { apple: 5 } }]
+inventory =   { 'name': 'owd', 'inventory': { 'apple': 5, 'orange': 10 } }, { 'name': 'dm', 'inventory': { 'banana': 5, 'orange': 10 } }
+order = { 'apple': 5, 'banana': 5, 'orange': 5 }
+output = {}
+for i in range (len(inventory)):
+    stock = list(inventory[i]['inventory'].keys()) # apple, orange
+    for item in stock :
+        # if item in stock and order[item] !=0 :
+            item_count = inventory[i]['inventory'].get(item) # 창고안의 재고 갯수만 가져오기 
+            if item_count   >= order.get(item) : # 주문보다 재고가 더 많을 경우 
+                output[item] = order[item]
+                # item_count  -= order[item]
+                # order[item]  = 0 
+                print (output)
